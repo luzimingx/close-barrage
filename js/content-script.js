ba71d453d1d473d064467478445aa8516dcd28fa
luziming <luzimingx@gmail.com>
2020-06-17 21:16:36 +0800

    0.0.2版本，支持虎牙平台

--- js/content-script.js
+++ js/content-script.js
@@ -1,13 +1,41 @@
+var host = location.host.split('.')[1],
+    hosts = ['douyu', 'huya', 'huomao'];
+
+var selectors = {
+    douyu: {
+        layout: ['.layout-Player-video'],
+        toolbar: ['#js-player-toolbar'],
+        barrage: ['\[title="关闭弹幕"\]'],
+        fullScreen: ['\[title="全屏"\]'],
+        quality: ['\[value="画质 "\]', 'nextElementSibling']
+    },
+    huya: {
+        layout: ['.room-player'],
+        toolbar: ['#player-gift-wrap'],
+        barrage: ['\[title="关闭弹幕"\]'],
+        fullScreen: ['\[title="全屏"\]'],
+        // fullScreen: ['#player-fullscreen-btn'],
+        quality: ['.player-videotype-list']
+    },
+    huomao: {
+        layout: ['#playing-box-root'],
+        toolbar: ['.live-bottom-bigbox'],
+        barrage: ['.danmu_state_state_notice'],
+        fullScreen: ['.full-screen'],
+        quality: ['.h5player_bitrate_change']
+    }
+};
+selectors = selectors[host];
+
+// 在左上角创建全屏按钮
+createFullScreenButton();
 // 隐藏礼物工具栏
-click('#js-player-toolbar', dom => dom.style.opacity = '0');
+click(selectors.toolbar, dom => dom.style.opacity = '0');
 // 关闭弹幕
-click("[title='关闭弹幕']", dom => dom.click());
-// 窗口全屏 没有效果
-click("[title='窗口全屏']", dom => dom.click());
+click(selectors.barrage, dom => dom.click());
 // 画质最佳
-click("[value='画质 ']", dom => {
-    var ul = dom.nextElementSibling;
-    ul.firstElementChild.click();
+click(selectors.quality, dom => {
+    dom && dom.firstElementChild.click();
 });
 
 function click(selector, suc, fail, maxTime) {
@@ -16,7 +44,8 @@ function click(selector, suc, fail, maxTime) {
         dom;
     var timer = setInterval(function() {
         time++;
-        dom = document.querySelector(selector);
+        dom = getElement(selector[0]);
+        dom && selector[1] && (dom = dom[selector[1]]);
         if (dom) {
             suc && typeof suc === 'function' && suc(dom);
             return clearInterval(timer);
@@ -26,4 +55,29 @@ function click(selector, suc, fail, maxTime) {
             return clearInterval(timer);
         }
     }, 500);
+}
+
+// 在左上角创建全屏按钮
+function createFullScreenButton() {
+    var layout = getElement(selectors.layout[0]);
+    layout.style.position = 'relative';
+    var button = createElement('div', {
+        style: 'width: 80px;height:80px;background:red;position:absolute;top:0;left:0'
+    });
+    button.addEventListener('click', function() {
+        click(selectors.fullScreen, dom => dom.click());
+    });
+    layout.appendChild(button);
+}
+// 创建dom
+function createElement(tag, attrs) {
+    var elem = document.createElement(tag);
+    for (var key in attrs) {
+        elem.setAttribute(key, attrs[key]);
+    }
+    return elem;
+}
+// 获取dom
+function getElement(selector) {
+    return document.querySelector(selector);
 }
