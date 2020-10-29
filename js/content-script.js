344f780cb8f295ff985f05e305410861bc66384b
luziming <luzimingx@gmail.com>
2020-10-29 16:43:28 +0800

    处理一些细节问题，删除火猫平台的支持

--- js/content-script.js
+++ js/content-script.js
@@ -1,36 +1,58 @@
 var host = location.host.split('.')[1],
-    hosts = ['douyu', 'huya', 'huomao'];
+    hosts = ['douyu', 'huya'];
 
 var selectors = {
     douyu: {
-        layout: ['.layout-Player-video'],
-        toolbar: ['#js-player-toolbar'],
-        barrage: ['\[title="关闭弹幕"\]'],
-        fullScreen: ['\[title="全屏"\]'],
-        quality: ['\[value="画质 "\]', 'nextElementSibling']
+        layout: ['.layout-Player-video'], // 视频框
+        toolbar: ['#js-player-toolbar'], // 礼物栏
+        barrage: ['\[title="关闭弹幕"\]'], // 弹幕按钮
+        fullScreen: ['\[title="全屏"\]'], //  全屏按钮
+        pageScreen: ['\[title="网页全屏"\]'], // 网页全屏按钮
+        quitPageScreen: ['\[title="退出网页全屏"\]'], // 退出网页全屏按钮
+        asideToggle: ['.layout-Player-asidetoggleButton'], // 网页全屏后 收起边栏按钮
+        quality: ['\[value="画质 "\]', 'nextElementSibling'], // 清晰度按钮
     },
     huya: {
-        layout: ['.room-player'],
+        layout: ['.room-player-wrap'],
         toolbar: ['#player-gift-wrap'],
         barrage: ['\[title="关闭弹幕"\]'],
         fullScreen: ['\[title="全屏"\]'],
         // fullScreen: ['#player-fullscreen-btn'],
+        pageScreen: ['\[title="剧场模式"\]'],
+        quitPageScreen: ['\[title="退出剧场"\]'],
+        asideToggle: ['#player-fullpage-right-btn'],
         quality: ['.player-videotype-list']
     },
-    huomao: {
-        layout: ['#playing-box-root'],
-        toolbar: ['.live-bottom-bigbox'],
-        barrage: ['.danmu_state_state_notice'],
-        fullScreen: ['.full-screen'],
-        quality: ['.h5player_bitrate_change']
-    }
+    // huomao: {
+    //     layout: ['#playing-box-root'],
+    //     toolbar: ['.live-bottom-bigbox'],
+    //     barrage: ['.danmu_state_state_notice'],
+    //     fullScreen: ['.full-screen'],
+    //     quality: ['.h5player_bitrate_change']
+    // }
 };
 selectors = selectors[host];
 
 // 在左上角创建全屏按钮
-createFullScreenButton();
+createButton({
+    style: 'width: 50%;height:50%;position:absolute;top:0;left:0;background:red',
+    realButton: 'fullScreen'
+});
+// 在右上角创建全屏按钮
+createButton({
+    style: 'width: 50%;height:50%;position:absolute;top:0;right:0;background:green',
+    realButton: 'pageScreen',
+    cb: function() {
+        click(selectors.asideToggle, dom => dom.click());
+    },
+});
+// 在右下角创建退出全屏按钮
+createButton({
+    style: 'width: 50%;height:50%;position:absolute;bottom:0;right:0;background:yellow',
+    realButton: 'quitPageScreen'
+});
 // 隐藏礼物工具栏
-click(selectors.toolbar, dom => dom.style.opacity = '0');
+click(selectors.toolbar, dom => dom.parentNode.removeChild(dom));
 // 关闭弹幕
 click(selectors.barrage, dom => dom.click());
 // 画质最佳
@@ -57,15 +79,20 @@ function click(selector, suc, fail, maxTime) {
     }, 500);
 }
 
-// 在左上角创建全屏按钮
-function createFullScreenButton() {
+// 创建方便操作的隐形按钮：全屏、网页全屏
+function createButton(options) {
     var layout = getElement(selectors.layout[0]);
-    layout.style.position = 'relative';
+    if (!layout) {
+        return setTimeout(function() {
+            createButton(options);
+        }, 500);
+    }
     var button = createElement('div', {
-        style: 'width: 80px;height:80px;position:absolute;top:0;left:0'
+        style: options.style
     });
     button.addEventListener('click', function() {
-        click(selectors.fullScreen, dom => dom.click());
+        click(selectors[options.realButton], dom => dom.click());
+        options.cb && typeof options.cb === 'function' && options.cb();
     });
     layout.appendChild(button);
 }
